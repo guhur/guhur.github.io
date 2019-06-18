57e1b64c65ddec0a6944f9d82b9d38e2ea8ab07e
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2019-06-18 12:11:39 +0200

    edited /home/pierre-louis/src/guhur.github.io/nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,5 @@
+# Some tips to install your NVIDIA drivers and CUDA on Ubuntu for Pytorch
+
+If you are using Pytorch framework, chances are that you will struggle to install all dependencies correctly. 
+At least, it was my case. 
+
